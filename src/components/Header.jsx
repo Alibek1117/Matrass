ddf5254aeda4d8f3f3e063ae9922b9a848fa427b
Nurjon <nurjon2477@gmail.com>
2023-09-01 20:40:34 +0500

    reponse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,12 +77,34 @@ function Header() {
             <LogoIcon />
           </div>
           <div className="header__right flex items-center">
-            <button className="Icon-Header md:hidden sm:hidden" onClick={togglePopover}>
+            <button className="Icon-Header   xl:hidden lg:hidden" onClick={togglePopover}>
               <IconHeader />
             </button>
+              <nav className="header__naw  ">
+               <ul className="flex ">
+                 <li>
+                   <a href="#">Katalog</a>
+                 </li>
+                 <li>
+                   <a href="#">Aksiya</a>
+                 </li>
+                 <li>
+                   <a href="#">Biz haqimizda</a>
+                 </li>
+                 <li>
+                   <a href="#">Manzilimiz</a>
+                 </li>
+                 <li>
+                   <a href="#">Aloqa</a>
+                </li>
+                 <li>
+                   <a href="#">Admin</a>
+                 </li>
+               </ul>
+             </nav>
             {popoverOpen && (
               <div className="popover absolute top-14 right-0 bg-white border border-gray-300 shadow-md p-4 z-10 md:static md:w-auto md:bg-transparent md:border-none md:p-0">
-                <ul className="md:flex md:space-x-4 md:items-center">
+                <ul className=" md:space-x-4 md:items-center">
                   <li><a href="#katalog">Katalog</a></li>
                   <li><a href="#aksiya">Aksiya</a></li>
                   <li><a href="#aboutUs">Biz haqimizda</a></li>
@@ -90,14 +112,14 @@ function Header() {
                   <li><a href="#aloqa">Aloqa</a></li>
                   <li><a href="#">Admin</a></li>
                 </ul>
-                <div className="tel__number flex items-center mt-4">
+                <div className="tel__number flex items-center mt-4  md:hidden">
                   <TelIcon className="w-5 h-5 mr-2" />
-                  <span className="tel">+998 90 123 45 67</span>
+                  <span className="tel ">+998 90 123 45 67</span>
                 </div>
                 <button className="order__btn mt-4">Buyurtma berish</button>
               </div>
             )}
-            <div className="left__bottom mt-6 flex items-center justify-end md:justify-start">
+            <div className="left__bottom  ">
               <div className="tel__number flex items-center">
                 <TelIcon className="w-5 h-5 mr-2" />
                 <span className="tel">+998 90 123 45 67</span>
